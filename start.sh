010827b6bb13541a56618c512f54de8ce44e8244
TAMAGO55123 <tamago.55123@gmail.com>
2026-03-30 20:53:19 +0900

    更新スクリプトの作成

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
-git pull
+#!/bin/bash
+
+echo "Downloading latest SOB..."
+curl -L -o sob.zip https://github.com/TAMAGO55123/SOB/releases/latest/download/sob.zip
+
+echo "Extracting..."
+unzip -o sob.zip
+
+echo "Python Package Installing..."
 python -m pip install -r requirements.txt
+
+echo "Starting bot..."
 python main.py
